63c501b3f3a8e036d9cd7c802a2456ffe39b16f3
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-04-05 16:33:30 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Name-Entity-Recognition",
+    version="0.0.1",
+    author="JunaidARahat",
+    author_email="mjunaidrahat1990@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
