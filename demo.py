910784713702ed07f99edf44cf433cbd83469569
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-04-06 17:39:09 +0500

    data ingestion added

---
+++ demo.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from ner.exception import NerException
+from ner.pipeline.train_pipeline import TrainPipeline
+
+from ner.constants import *
+
+
+def training():
+    try:
+        train_pipeline = TrainPipeline()
+
+        train_pipeline.run_pipeline()
+
+    except Exception as e:
+        raise NerException(e, sys) from e
+
+
+if __name__ == "__main__":
+    training()
---
+++ ner/pipeline/train_pipeline.py
@@ -0,0 +1,53 @@
+import sys
+from ner.components.data_ingestion import DataIngestion
+from ner.configuration.gcloud import GCloud
+from ner.constants import *
+from ner.exception import NerException
+from ner.logger import logging
+
+from ner.entity.artifact_entity import (DataIngestionArtifacts)
+
+
+from ner.entity.config_entity import (DataIngestionConfig)
+
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.gcloud = GCloud()
+
+    
+
+
+    def start_data_ingestion(self) -> DataIngestionArtifacts:
+        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+        try:
+            logging.info("Getting the data from Google cloud storage")
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config, gcloud=self.gcloud
+            )
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the data from Google cloud storage")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise NerException(e, sys) from e
+        
+
+    
+
+
+
+
+      # This method is used to start the training pipeline
+    def run_pipeline(self) -> None:
+        try:
+            logging.info("Started Model training >>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            data_ingestion_artifact = self.start_data_ingestion()
+        
+        except Exception as e:
+            raise NerException(e, sys) from e
